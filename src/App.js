e0a004dc48b3e71d347f6270ab96023bd3e6bd72
TahsinSumiya <tahsinadity187@gmail.com>
2022-08-10 20:46:43 +0600

    category

--- src/App.js
+++ src/App.js
@@ -3,8 +3,39 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+// set to set all the unique values
+const allCategories = ['all',...new Set(items.map((item)=>item.category))];
+
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category=== "all"){
+      // passing all where all is seen all items will display
+      setMenuItems(items)  
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category)
+    // newItems means selected items
+     setMenuItems(newItems);  
+  }
+ 
+
+
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems}  />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
